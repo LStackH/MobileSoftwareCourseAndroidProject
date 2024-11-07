5dfb154775fb7a966ba140f54405d49eead2c2f2
lukas-honka <lukas.honka@student.lut.fi>
2024-11-07 17:40:50 +0200

    Added "on this day/ Wikipedia Summary" view for the MainActivity with functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.okhttp)          // OkHttp
+    implementation(libs.gson)            // Gson
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- app/src/main/java/com/example/mobilesoftwarecourseproject/MainActivity.java
+++ app/src/main/java/com/example/mobilesoftwarecourseproject/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.mobilesoftwarecourseproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +15,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Random;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView wikipediaSummary;
+    private CalendarView calendarView;
+    private TextView title;
+
+    private String googleBtnUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +47,123 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button secondActivityBtn = findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntent.putExtra("com.example.mobilesoftwarecourseproject.SOMETHING", "HELLO WORLD!");
+                startActivity(startIntent);
+
+            }
+        });
+
+        Button googleBtn = findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (googleBtnUrl != null) {
+                    openURL_Intent(googleBtnUrl);
+                } else {
+                    Toast.makeText(MainActivity.this, "No url for event", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        wikipediaSummary = findViewById(R.id.wikipediaSummary);
+        calendarView = findViewById(R.id.calendarView);
+        title = findViewById(R.id.title);
+
+        // Listen for date change on the calendarView
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // Call fetchWikipediaEvent with the selected month and day
+                fetchWikipediaEvent(month + 1, dayOfMonth);
+                title.setText("On this day (" + dayOfMonth + "." + (month+1) + "):" );
+            }
+        });
+
+        // Default for the current day
+        int current_month = Calendar.getInstance().get(Calendar.MONTH)+1;
+        int current_day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        title.setText("On this day (" + current_day + "." + current_month + "):" );
+        fetchWikipediaEvent(current_month, current_day);
+
     }
+
+
+
+    // Handles the wikipedia fetching
+    private void fetchWikipediaEvent(int month, int day) {
+        String url = "https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/" + month + "/" + day;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        runOnUiThread(() -> wikipediaSummary.setText("Loading content..."));
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(() -> wikipediaSummary.setText("Failed to load event."));
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful() && response.body() != null) {
+                    String responseData = response.body().string();
+                    JsonObject json = JsonParser.parseString(responseData).getAsJsonObject();
+                    JsonArray events = json.getAsJsonArray("events");
+                    if (events.size() > 0) {
+                        // Generate random num to pick from all the events in the json array
+                        Random rand = new Random();
+                        int random_number = rand.nextInt(events.size());
+
+                        JsonObject firstEvent = events.get(random_number).getAsJsonObject();
+                        String description = firstEvent.get("text").getAsString();
+
+                        googleBtnUrl = null;
+
+                        JsonArray pages = firstEvent.getAsJsonArray("pages");
+                        if (pages != null && pages.size() > 0) {
+                            JsonObject firstPage = pages.get(0).getAsJsonObject();
+                            JsonObject contentUrls = firstPage.getAsJsonObject("content_urls");
+                            if (contentUrls != null) {
+                                JsonObject mobile = contentUrls.getAsJsonObject("mobile");
+                                googleBtnUrl = mobile.get("page").getAsString();
+                            }
+                        }
+
+                        // Update UI with the description
+                        runOnUiThread(() -> wikipediaSummary.setText(description));
+                    } else {
+                        googleBtnUrl = null;
+                        runOnUiThread(() -> wikipediaSummary.setText("No events for this date."));
+                    }
+                } else {
+                    googleBtnUrl = null;
+                    runOnUiThread(() -> wikipediaSummary.setText("Failed to load event."));
+                }
+            }
+        });
+    }
+
+    // Opens the given URL, outside the application
+    private void openURL_Intent(String url){
+
+        Uri address = Uri.parse(url);
+
+        Intent gotoURL = new Intent(Intent.ACTION_VIEW, address);
+        if (gotoURL.resolveActivity(getPackageManager()) != null) {
+            startActivity(gotoURL);
+        } else {
+            // Inform the user that no browser is available
+            Toast.makeText(MainActivity.this, "No browser available to open the link.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
